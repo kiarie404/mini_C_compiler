91900d50b9ed59a6f82967acab8a842643c0d5ff
kiarie404 <kiarie404@gmail.com>
2022-04-02 14:30:44 -0700

    Full test

--- lexer/lexer_with_sly/Test.c
+++ lexer/lexer_with_sly/Test.c
@@ -1,3 +1,13 @@
 int sum(int num1, int num2){
+  bool result = false;
+  bool result2 = TRUE;
+  float real = 5.90;
+  void voider.
+
+  3%2=14 < 453
+
+  int weather = !(windy && sunny) || (rainy) 
   return num + num2 + 3.950 * (15 / 3);
+  *r45
+  break
 }
