e1ce2df6f6711454c3c2934c53b61d893441347f
kiarie404 <kiarie404@gmail.com>
2022-04-07 05:46:19 -0700

    update

--- syntax_parser/hard_coded_LL1_parser/LL1_grammar_creator/read_me.py
+++ syntax_parser/hard_coded_LL1_parser/LL1_grammar_creator/read_me.py
@@ -2,3 +2,7 @@ This module consumes a BNF and produces an LL1 grammar :
   The LL1 grammar is a grammar that does not have :
     - Left factoring.
     - Left recursion.
+
+The files in this module are :
+    - LL1_grammar_creator.py - consumes a BNF and produces an equivalent LL1-grammar (no left factoring or left recursion)
+    - LL1_grammar_checker.py - checks if the output from the LL1_grammar_creator.py is okay.
