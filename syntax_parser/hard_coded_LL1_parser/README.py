8e112e203255560f345e4955e22d0b7e01a24511
kiarie404 <kiarie404@gmail.com>
2022-04-12 03:46:40 -0700

    Update README.py

--- syntax_parser/hard_coded_LL1_parser/README.py
+++ syntax_parser/hard_coded_LL1_parser/README.py
@@ -1,20 +1,34 @@
-If we are going to hardcode, then we need to 
-  1. Make this code as modular as posssible
-  2. Agree on the pseudocode of each module before assigning them to anyone
-  3. Agree on the API function calls from each module
-  4. Any one who does not participate in the code... flunked out of the group.
+# proposition of module arrangement by kiarie404
+*** maybe we should use yaml instead of json in order to allow comments within the passed json text ***
+*** but this can esily be solved by unanimously agreeing on the json format of various files *****
+
+Let the LL1 parser have the following modules :
   
-My proposition :
-  We meet at 2:00pm : where we will -
-      - design the modules
-      - agree on the pseudocode
-      - assign tasks
+  - Input files
+      - BNF.txt
+      - token_stream.txt
+      
+  - Preparation_module
+      - BNF_to_JSON_converter.py ---- > converts BNF.txt into a  BNF.json file
+      - token_stream_to_JSON_converter.py ---- > converts token_stream.txt into a token_stream.json file
+          
+       output files of the Preparation module
+          - BNF.json
+          - token_stream.json
+  - LL1_grammar_creator
+      - Left_recursion_remover.py ----> consumes BNF.json and outputs BNF_without_left_recursion.json
+      - left_factoring_remover.py ----> consumes BNF_without_left_recursion.json and outputs full_LL1_grammar_BNF.json
+      
+  - LL1_table_creator
+      - generate_first_sets.py ---> consumes the full_LL1_grammar_BNF.json and outputs first_sets.json
+      - generate_follow_sets.py ---> consumes the first_sets.json + full_LL1_grammar_BNF.json  and outputs full_LL1_symbol_table.json
+      
+  - parser
+      - parser.py - consumes the token_stream.json + full_LL1_symbol_table.json and outputs syntax_tree.json
+      - tree_generator.py ---> consumes the syntax_tree.json and displays a visual tree structure : (optional...since we can use online tree generators )
   
-  From here on we meet everyday, to check where we are...(a brief 15min max meeting, so be in time - ( not on time ))
-    - To check where the person assigned is
-    - discuss how the modules may change
-    
-  We have to finish this on Sunday hopefully, 
-  No participation == Out of group . Sad but necessary
-   
+
+# proposition by : <insert your name here>
+
+
   
