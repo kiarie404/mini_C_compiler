4e4033cc5cdf6f2d4f4fb866cd607ef4b0fe149c
kiarie404 <kiarie404@gmail.com>
2022-04-12 10:52:30 -0700

    elaborate actions

--- syntax_parser/parser_with_sly/example_implementation_on_a_small_grammar/grammar.py
+++ syntax_parser/parser_with_sly/example_implementation_on_a_small_grammar/grammar.py
@@ -1,10 +1,12 @@
-statement ::= NAME ASSIGN expr
-           |  expr
+# rule_index = 0
+statement ::= NAME ASSIGN expr  # index = 0.1
+           |  expr # index = 0.2
 
-expr      ::= expr PLUS expr
-           |  expr MINUS expr
-           |  expr TIMES expr
-           |  expr DIVIDE expr
-           |  LPAREN expr RPAREN
-           |  NUMBER
-           |  NAME
+# rule_index = 1
+expr      ::= expr PLUS expr    # index = 1.1
+           |  expr MINUS expr   # index = 1.2
+           |  expr TIMES expr   # index = 1.3
+           |  expr DIVIDE expr  # index = 1.4
+           |  LPAREN expr RPAREN    # index = 1.5
+           |  NUMBER    # index = 1.6
+           |  NAME      # index = 1.7
---
+++ syntax_parser/parser_with_sly/grammar.md
@@ -0,0 +1,8 @@
+assignment :  ID EQUALS NUMBER
+           |  ID EQUALS expr
+
+expr       : expr PLUS expr
+           | expr MINUS expr
+           | expr TIMES expr
+           | expr DIVIDE expr
+           | LPAREN expr RPAREN
