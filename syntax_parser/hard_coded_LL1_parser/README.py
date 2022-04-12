282109b09f21ece6d4e3166db61c0c868fc80634
kiarie404 <kiarie404@gmail.com>
2022-04-12 03:03:19 -0700

    Update README.py

--- syntax_parser/hard_coded_LL1_parser/README.py
+++ syntax_parser/hard_coded_LL1_parser/README.py
@@ -1,2 +1,20 @@
-If we are going to hardcode, then we need to make this code
-  1. As modular as posssible
+If we are going to hardcode, then we need to 
+  1. Make this code as modular as posssible
+  2. Agree on the pseudocode of each module before assigning them to anyone
+  3. Agree on the API function calls from each module
+  4. Any one who does not participate in the code... flunked out of the group.
+  
+My proposition :
+  We meet at 2:00pm : where we will -
+      - design the modules
+      - agree on the pseudocode
+      - assign tasks
+  
+  From here on we meet everyday, to check where we are...(a brief 15min max meeting, so be in time - ( not on time ))
+    - To check where the person assigned is
+    - discuss how the modules may change
+    
+  We have to finish this on Sunday hopefully, 
+  No participation == Out of group . Sad but necessary
+   
+  
