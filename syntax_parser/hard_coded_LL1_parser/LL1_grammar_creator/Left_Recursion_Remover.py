5b3ad14ee528d28782e8b25210c3cc9a6fe4f083
Isabella Abuor <70433606+ISABELLAKORDEI@users.noreply.github.com>
2022-04-16 00:09:32 +0300

    Add files via upload
    And yes the are two of them one written by me and the other one re-formatted but it was the one that followed the algorithm nicely even with # as epsilon

--- syntax_parser/hard_coded_LL1_parser/LL1_grammar_creator/Left_Recursion_Remover.py
+++ syntax_parser/hard_coded_LL1_parser/LL1_grammar_creator/Left_Recursion_Remover.py
@@ -5,73 +5,46 @@ def getBNFJSON(Hardcoded_LL1BNFjson):
     with open('Hardcoded_LL1\BNF.json', 'r') as fp:
         return json.load(fp)
 
-#looking and removing left rescursion both directly and inderectly according to the algo ya https://web.cs.wpi.edu/~kal/images/PLT/PLTelralgorithm.gif
-#gramA= dict
-def removeDirectLR(gramA, A):
-	temp = gramA[A]
-	tempCr = []
-	tempInCr = []
-	for i in temp:
-		if i[0] == A:
-			#tempInCr.append(i[1:]) (store)
-			tempInCr.append(i[1:]+[A+"'"])
-		else:
-			#tempCr.append(i)
-			tempCr.append(i+[A+"'"])
-	tempInCr.append(["e"])
-	gramA[A] = tempCr
-	gramA[A+"'"] = tempInCr
-	return gramA
+def removeLeftRecursion(rulesDict):
+    # to store the new rules to be added in the file
+    store = {}
+    # traverse over rules
+    #divide the rules into two the ones that have left recursion and the ones without
+    for lhs in rulesDict:
+        alphaRules = []
+        betaRules = []
+        # get righthandside for current lefthandside
+        allrhs = rulesDict[lhs]
+        for subrhs in allrhs:
+            if subrhs[0] == lhs:
+                alphaRules.append(subrhs[1:])
+            else:
+                betaRules.append(subrhs)
+        # now form two new rules
+        if len(alphaRules) != 0:
+            # to generate new unique symbol
+            lhs_ = lhs + "'"
+            while (lhs_ in rulesDict.keys()) \
+                    or (lhs_ in store.keys()):
+                lhs_ += "'"
+            # make beta rule and alpha rule
+            for b in range(0, len(betaRules)):
+                betaRules[b].append(lhs_)
+            rulesDict[lhs] = betaRules
 
-
-def checkForIndirect(gramA, a, ai):
-	if ai not in gramA:
-		return False
-	if a == ai:
-		return True
-	for i in gramA[ai]:
-		if i[0] == ai:
-			return False
-		if i[0] in gramA:
-			return checkForIndirect(gramA, a, i[0])
-	return False
-
-def rep(gramA, A):
-	temp = gramA[A]
-	newTemp = []
-	for i in temp:
-		if checkForIndirect(gramA, A, i[0]):
-			t = []
-			for k in gramA[i[0]]:
-				t=[]
-				t+=k
-				t+=i[1:]
-				newTemp.append(t)
-
-		else:
-			newTemp.append(i)
-	gramA[A] = newTemp
-	return gramA
-
-def rem(gram):
-	c = 1
-	conv = {}
-	gramA = {}
-	revconv = {}
-	for j in gram:
-		conv[j] = "A"+str(c)
-		gramA["A"+str(c)] = []
-		c+=1
-
-	for i in gram:
-		for j in gram[i]:
-			temp = []
-			for k in j:
-				if k in conv:
-					temp.append(conv[k])
-				else:
-					temp.append(k)
-			gramA[conv[i]].append(temp)
+            for a in range(0, len(alphaRules)):
+                alphaRules[a].append(lhs_)
+            alphaRules.append(['#'])
+            # store in temp
+            store[lhs_] = alphaRules
+    # - after removing left recursion
+    for left in store:
+        rulesDict[left] = store[left]
+    return rulesDict
 #Writing on the BNF.json(updating it)
-
-#Outputting the BNF.json without Left rescursion to json file
+def pasteBNFJSON(Hardcoded_LL1BNFjson):
+    with open('Hardcoded_LL1\BNF.json', 'w') as f:
+        return json.dump(store,f,ensure_ascii=False)
+        f.close()
+#so basically here is where the problem is,I want to convert it into a new json file called BNF_without_recursion.json
+# instead on writing on the old one (bnf.json) so we can have two files 
